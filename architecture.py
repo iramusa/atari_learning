0dead9260834221baaa858889ca32eef698bf6b7
Marian Andrecki <marian.andrecki@gmail.com>
2017-02-13 11:53:42 +0000

    Added network constructors and structure definitions for autoencoder.

---
+++ architecture.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+"""
+Class for large network with multiple branches, cost functions, training stages.
+"""
+
+# imports
+from keras.models import Model
+from keras.layers import Input, Dense, Convolution2D, Deconvolution2D, MaxPooling2D, UpSampling2D, Merge, LSTM
+# import keras
+from keras.utils.visualize_util import plot
+
+import structures
+
+
+FOLDER_MODELS = 'models'
+
+def make_trainable(model, trainable):
+    """
+    Freeze or unfreeze network weights
+    :param model: particular model (layers of neural net)
+    :param trainable: if true freeze, else unfreeze
+    :return: None
+    """
+    model.trainable = trainable
+    for l in model.layers:
+        l.trainable = trainable
+
+
+class MultiNetwork(object):
+    def __init__(self, **kwargs):
+        self.structure = kwargs.get('structure', structures.DEFAULT_STRUCTURE)
+
+        # branches of network
+        self.encoder = None
+        self.decoder = None
+        self.physics_predictor = None
+        self.action_mapper = None
+        self.action_predictor = None
+        self.state_sampler = None
+
+        self.screen_discriminator = None
+        self.state_discriminator = None
+
+        # full networks
+        self.autoencoder_gen = None
+        self.autoencoder_critic = None
+        self.autoencoder_gan = None
+
+        self.screen_predictor_g = None
+        self.screen_predictor_d = None
+
+        self.state_assigner = None
+        self.future_sampler_g = None
+        self.future_sampler_d = None
+
+        self.build_branches()
+        self.build_networks()
+
+    def build_branches(self):
+        self.encoder = self.build_branch(structures.ENCODER)
+        self.decoder = self.build_branch(structures.DECODER)
+        self.screen_discriminator = self.build_branch(structures.SCREEN_DISCRIMINATOR)
+
+        # self.physics_predictor = self.build_physics_predictor()
+        # self.action_mapper = self.build_action_mapper()
+        # self.action_predictor = self.build_action_predictor()
+        #
+        # self.state_sampler = self.build_state_sampler()
+        # self.decoder = self.build_decoder()
+
+    def build_networks(self):
+        self.build_autoencoder()
+
+    def build_branch(self, structure):
+        input_shape = structure.get('input_shape')
+        output_shape = structure.get('output_shape')
+        name = structure.get('name')
+
+        layers = structure.get('layers')
+
+        input_layer = Input(shape=input_shape)
+        x = input_layer
+
+        for layer in layers:
+            layer_constructor = layer.get('type')
+            pos_args = layer.get(structures.POSITIONAL_ARGS, [])
+            key_args = layer.get(structures.KEYWORD_ARGS, {})
+            print('Building: ', layer_constructor, pos_args, key_args)
+            x = layer_constructor(*pos_args, **key_args)(x)
+
+        branch = Model(input_layer, x, name=name)
+        branch.summary()
+        if not branch.output_shape[1:] == output_shape:
+            raise ValueError('Bad output shape! Expected: {0} Actual: {1}'.format(output_shape, branch.output_shape))
+
+        plot(branch, to_file='{0}/{1}.png'.format(FOLDER_MODELS, name), show_layer_names=True, show_shapes=True)
+
+        return branch
+
+    def build_autoencoder(self):
+        input_img = Input(shape=structures.INPUT_IMAGE_SHAPE)
+        z = self.encoder(input_img)
+        screen_recon = self.decoder(z)
+        screen_disc = self.screen_discriminator(z)
+
+        self.autoencoder_gen = Model(input_img, screen_recon)
+        self.autoencoder_gen.summary()
+        plot(self.autoencoder_gen, to_file='{0}/{1}.png'.format(FOLDER_MODELS, 'autoencoder_gen'), show_layer_names=True,
+             show_shapes=True)
+
+        self.autoencoder_critic = Model(input_img, screen_disc)
+        self.autoencoder_critic.summary()
+        plot(self.autoencoder_critic, to_file='{0}/{1}.png'.format(FOLDER_MODELS, 'autoencoder_critic'), show_layer_names=True,
+             show_shapes=True)
+
+        z = self.encoder(input_img)
+        screen_recon = self.decoder(z)
+        z = self.encoder(screen_recon)
+        screen_disc = self.screen_discriminator(z)
+
+        self.autoencoder_gan = Model(input_img, screen_disc)
+        self.autoencoder_gan.summary()
+        plot(self.autoencoder_gan, to_file='{0}/{1}.png'.format(FOLDER_MODELS, 'autoencoder_gan'),
+             show_layer_names=True,
+             show_shapes=True)
+
+    def build_physics_predictor(self):
+        return self
+
+    def build_action_predictor(self):
+        return self
+
+    def build_state_sampler(self):
+        return self
+
+    def train_network(self):
+        # stages:
+        # 1) encoder/decoder
+        #
+
+
+
+        return self
+
+    def show_reconstruction(self):
+        return self
+
+    def show_predictions(self):
+        return self
+
+if __name__ == '__main__':
+    mn = MultiNetwork()
+
+
---
+++ structures.py
@@ -0,0 +1,243 @@
+from keras.models import Model
+from keras.layers import Input, Dense, Convolution2D, Deconvolution2D, MaxPooling2D,\
+    UpSampling2D, Merge, LSTM, Flatten, ZeroPadding2D, Reshape
+
+
+IMAGE_SIZE_X = 160
+IMAGE_SIZE_Y = 210
+IMAGE_CHANNELS = 3
+
+INPUT_IMAGE_SHAPE = (IMAGE_SIZE_X, IMAGE_SIZE_Y, IMAGE_CHANNELS)
+
+V_SIZE = 256
+A_MAP_SIZE = 100
+S_SIZE = 200
+
+# DENSE_SIZE = 2048
+
+POSITIONAL_ARGS = 'pos_args'
+KEYWORD_ARGS = 'key_args'
+
+ENCODER = {
+        'name': 'conv4',
+        'input_shape': INPUT_IMAGE_SHAPE,
+        'output_shape': (V_SIZE,),
+        'layers': [
+            {
+                'type': ZeroPadding2D,
+                KEYWORD_ARGS: {
+                    'padding': (1, 0)
+                }
+
+            },
+            {
+                'type': Convolution2D,
+                POSITIONAL_ARGS: [32, 8, 8],
+                'nbfilter': 32,
+                'nbrow': 8,
+                'nbcol': 8,
+                KEYWORD_ARGS : {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    # 'border_mode': 'same'
+                }
+            },
+            {
+                'type': ZeroPadding2D,
+                KEYWORD_ARGS: {
+                    'padding': (1, 1)
+                }
+
+            },
+            {
+                'type': Convolution2D,
+                POSITIONAL_ARGS: [64, 6, 6],
+                'nbfilter': 64,
+                'nbrow': 6,
+                'nbcol': 6,
+                KEYWORD_ARGS : {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    # 'border_mode': 'same'
+                }
+            },
+            {
+                'type': ZeroPadding2D,
+                KEYWORD_ARGS: {
+                    'padding': (1, 1)
+                }
+
+            },
+            {
+                'type': Convolution2D,
+                POSITIONAL_ARGS: [64, 6, 6],
+                'nbfilter': 64,
+                'nbrow': 6,
+                'nbcol': 6,
+                KEYWORD_ARGS: {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    # 'border_mode': 'same'
+                }
+            },
+            # {
+            #     'type': MaxPooling2D,
+            #     POSITIONAL_ARGS: [(2, 2)],
+            #     'pool_size': (2, 2),
+            #     KEYWORD_ARGS: {
+            #         'border_mode': 'same'
+            #     }
+            # },
+            {
+                'type': Convolution2D,
+                POSITIONAL_ARGS: [64, 4, 4],
+                'nbfilter': 64,
+                'nbrow': 4,
+                'nbcol': 4,
+                KEYWORD_ARGS: {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    # 'border_mode': 'same'
+                }
+            },
+            {
+                'type': Flatten,
+            },
+            {
+                'type': Dense,
+                POSITIONAL_ARGS: [V_SIZE],
+                'output_dim': V_SIZE,
+                KEYWORD_ARGS: {
+                    'activation': 'relu',
+                }
+            },
+        ],
+    }
+
+
+DECODER = {
+        'name': 'deconv4',
+        'input_shape': (V_SIZE,),
+        'output_shape': INPUT_IMAGE_SHAPE,
+        'layers': [
+            {
+                'type': Dense,
+                POSITIONAL_ARGS: [8*11*64],
+                'output_dim': 8*11*64,
+                KEYWORD_ARGS: {
+                    'activation': 'relu',
+                }
+            },
+            {
+                'type': Reshape,
+                POSITIONAL_ARGS: [(8, 11, 64)],
+                'shape': (8, 11, 64),
+            },
+            {
+                'type': Deconvolution2D,
+                POSITIONAL_ARGS: [64, 4, 4],
+                'nbfilter': 32,
+                'nbrow': 8,
+                'nbcol': 8,
+                KEYWORD_ARGS : {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    'output_shape': (-1, 18, 24, 64),
+                    # 'border_mode': 'same'
+                }
+            },
+            {
+                'type': ZeroPadding2D,
+                KEYWORD_ARGS: {
+                    'padding': (1, 1)
+                }
+
+            },
+            {
+                'type': Deconvolution2D,
+                POSITIONAL_ARGS: [64, 6, 6],
+                'nbfilter': 64,
+                'nbrow': 6,
+                'nbcol': 6,
+                KEYWORD_ARGS : {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    'output_shape': (-1, 38, 50, 64),
+
+                    # 'border_mode': 'same'
+                }
+            },
+            {
+                'type': ZeroPadding2D,
+                KEYWORD_ARGS: {
+                    'padding': (1, 1)
+                }
+
+            },
+            {
+                'type': Deconvolution2D,
+                POSITIONAL_ARGS: [64, 6, 6],
+                'nbfilter': 64,
+                'nbrow': 6,
+                'nbcol': 6,
+                KEYWORD_ARGS: {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    'output_shape': (-1, 78, 102, 64),
+
+                    # 'border_mode': 'same'
+                }
+            },
+            {
+                'type': ZeroPadding2D,
+                KEYWORD_ARGS: {
+                    'padding': (0, 1)
+                }
+
+            },
+            # {
+            #     'type': MaxPooling2D,
+            #     POSITIONAL_ARGS: [(2, 2)],
+            #     'pool_size': (2, 2),
+            #     KEYWORD_ARGS: {
+            #         'border_mode': 'same'
+            #     }
+            # },
+            {
+                'type': Deconvolution2D,
+                POSITIONAL_ARGS: [3, 8, 8],
+                'nbfilter': 64,
+                'nbrow': 4,
+                'nbcol': 4,
+                KEYWORD_ARGS: {
+                    'subsample': (2, 2),
+                    'activation': 'relu',
+                    'output_shape': (-1, 160, 210, 3),
+                    # 'border_mode': 'same'
+                }
+            },
+        ],
+    }
+
+SCREEN_DISCRIMINATOR = {
+        'name': 'scr_disc',
+        'input_shape': (V_SIZE,),
+        'output_shape': (1,),
+        'layers': [
+            {
+                'type': Dense,
+                POSITIONAL_ARGS: [1],
+                'output_dim': 1,
+                KEYWORD_ARGS: {
+                    'activation': 'relu',
+                }
+            },
+        ],
+    }
+
+
+
+DEFAULT_STRUCTURE = {
+    'encoder': ENCODER,
+    'decoder': DECODER,
+}
